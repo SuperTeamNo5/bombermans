305f0abe5a2a14f924d2bd9f1b988a87098d7f00
Hankyung <14130029@st.hcmuaf.edu.vn>
2018-04-08 10:53:10 +0700

    create class Position
    just create position class for fun

---
+++ src/com/game/bomberman/model/Position.java
@@ -0,0 +1,27 @@
+package com.game.bomberman.model;
+
+public class Position {
+	private int xCoordinate;
+	private int yCoordinate;
+
+	public Position(int xCoordinate, int yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
+
+	public int getxCoordinate() {
+		return xCoordinate;
+	}
+
+	public void setxCoordinate(int xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public int getyCoordinate() {
+		return yCoordinate;
+	}
+
+	public void setyCoordinate(int yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+}
